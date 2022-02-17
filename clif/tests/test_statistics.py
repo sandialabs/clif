0e03ec1628fee5360407eabcedf7531e15372371
Kamaljit Chowdhary <kennychowdhary@gmail.com>
2022-02-16 22:26:34 -0800

    new tests for stats class

--- clif/tests/test_statistics.py
+++ clif/tests/test_statistics.py
@@ -17,18 +17,20 @@ class TestStationarityTests(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self.n = 1000
+        self.pvalue = 0.01
+        self.rn = np.random.RandomState(2342)
         # stocastic_data[:, 0] is a stocastic gaussian process. This should always be stationary.
-        self.stocastic_data = np.random.randn(self.n, 3)
-        # stocastic_data[:, 1] is (y_t = y_{t-1} * error) (error representing by data[:, 0])
+        self.stochastic_data = self.rn.randn(self.n, 3)
+        # stochastic_data[:, 1] is (y_t = y_{t-1} * error) (error representing by data[:, 0])
         #   This represents a heteroscadistic process that *should* fail the test but it currently does not. The test does not test for scadisticity right now.
-        # stocastic_data[:, 1] is (y_t = y_{t-1} + error) (error representing by data[:, 0])
+        # stochastic_data[:, 1] is (y_t = y_{t-1} + error) (error representing by data[:, 0])
         #   This represents a seasonal process, which is nonstationary.
         for t in range(self.n):
-            self.stocastic_data[t, 1] = (
-                self.stocastic_data[t - 1, 1] * self.stocastic_data[t, 0]
+            self.stochastic_data[t, 1] = (
+                self.stochastic_data[t - 1, 1] * self.stochastic_data[t, 0]
             )
-            self.stocastic_data[t, 2] = (
-                self.stocastic_data[t - 1, 2] + self.stocastic_data[t, 0]
+            self.stochastic_data[t, 2] = (
+                self.stochastic_data[t - 1, 2] + self.stochastic_data[t, 0]
             )
         # trend_series is a linearly increasing function representing a simple changing mean.
         #   It is difference stationary, so the ADF test return stationary and the KPSS test return nonstationary.
@@ -38,39 +40,77 @@ class TestStationarityTests(unittest.TestCase):
         #   Due to the difference in the results from ADF test and KPSS test, it can be inferred that the series is trend stationary and not strict stationary.
         self.sun_data = sm.datasets.sunspots.load_pandas().data["SUNACTIVITY"].values
 
-    def test_stationarity_tests(self):
-        trend_series_result = clif.statistics.stationarity(
-            self.trend_series, 0.01, verbosity=0
+    def test_stationarity_for_simple_linear_trend(self):
+        time_series = 1 + 0.1 * np.sort(self.rn.rand(self.n))
+        pvalue = self.pvalue
+        stest_adfuller = clif.statistics.StationarityTest(
+            test="adfuller", pvalue=pvalue
         )
+        stest_adfuller.fit(time_series)
         assert (
-            trend_series_result[0] == True and trend_series_result[1] == False
-        ), "Test on linear trend is incorrect."
+            stest_adfuller.is_stationary is False
+        ), "linear time-series should not be stationary using the adfuller test."
+        stest_kpss = clif.statistics.StationarityTest(test="kpss", pvalue=pvalue)
+        stest_kpss.fit(time_series)
+        assert (
+            stest_kpss.is_stationary is False
+        ), "linear time-series should not be stationary using the kpss test."
 
-        stationary_data_result = clif.statistics.stationarity(
-            self.stocastic_data[:, 0], 0.01, verbosity=0
+    def test_stationarity_for_simple_random_trend(self):
+        time_series = 1 + 0.1 * (self.rn.rand(self.n))
+        pvalue = self.pvalue
+        stest_adfuller = clif.statistics.StationarityTest(
+            test="adfuller", pvalue=pvalue
         )
+        stest_adfuller.fit(time_series)
         assert (
-            stationary_data_result[0] == True and stationary_data_result[1] == True
-        ), "Test on stationary data is incorrect."
+            stest_adfuller.is_stationary is True
+        ), "linear time-series should be stationary using the adfuller test."
+        stest_kpss = clif.statistics.StationarityTest(test="kpss", pvalue=pvalue)
+        stest_kpss.fit(time_series)
+        assert (
+            stest_kpss.is_stationary is True
+        ), "linear time-series should be stationary using the kpss test."
 
-        # Not implemented:
-        # heteroscedastic_result = clif.statistics.stationarity(
-        #     stocastic_data[:, 1], 0.01, verbosity=0
-        # )
-        # assert (
-        #     heteroscedastic_result[0] == True
-        #     and heteroscedastic_result[1] == True
-        #     and heteroscedastic_result[2] == False
-        # ), "Test on heteroscedastic data is incorrect"
-
-        seasonal_series_result = clif.statistics.stationarity(
-            self.stocastic_data[:, 2], 0.01, verbosity=0
+    def test_stationarity_for_sun_spot_data(self):
+        time_series = self.sun_data
+        pvalue = self.pvalue
+        stest_adfuller = clif.statistics.StationarityTest(
+            test="adfuller", pvalue=pvalue
         )
+        stest_adfuller.fit(time_series)
         assert (
-            seasonal_series_result[0] == False and seasonal_series_result[1] == False
-        ), "Test on seasonal data is incorrect."
+            stest_adfuller.is_stationary is False
+        ), "linear time-series should be not be stationary using the adfuller test."
+        stest_kpss = clif.statistics.StationarityTest(test="kpss", pvalue=pvalue)
+        stest_kpss.fit(time_series)
+        assert (
+            stest_kpss.is_stationary is True
+        ), "linear time-series should be stationary using the kpss test."
 
-        sun_data_result = clif.statistics.stationarity(self.sun_data, 0.01, verbosity=0)
+    def test_stationarity_for_artificial_stochastic_process(self):
+        time_series = self.stochastic_data[:, 2]
+        pvalue = self.pvalue
+        stest_adfuller = clif.statistics.StationarityTest(
+            test="adfuller", pvalue=pvalue
+        )
+        stest_adfuller.fit(time_series)
         assert (
-            sun_data_result[0] == False and sun_data_result[1] == True
-        ), "Test on trend-only stationary data is incorrect."
+            stest_adfuller.is_stationary is False
+        ), "linear time-series should be not be stationary using the adfuller test."
+        stest_kpss = clif.statistics.StationarityTest(test="kpss", pvalue=pvalue)
+        stest_kpss.fit(time_series)
+        assert (
+            stest_kpss.is_stationary is False
+        ), "linear time-series should not be stationary using the kpss test."
+
+    # def test_stationarity_for_heteraskedastic_time_series(self):
+    #     # Not implemented:
+    #     # heteroscedastic_result = clif.statistics.StationarityTest(
+    #     #     stocastic_data[:, 1], 0.01, verbosity=0
+    #     # )
+    #     # assert (
+    #     #     heteroscedastic_result[0] == True
+    #     #     and heteroscedastic_result[1] == True
+    #     #     and heteroscedastic_result[2] == False
+    #     # ), "Test on heteroscedastic data is incorrect"
